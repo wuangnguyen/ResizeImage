a9b5842e5e774ad2a91c6ba7531acd298ede473c
Tommy Nguyen <quang.nnd.hcmus@gmail.com>
2017-02-06 09:30:47 +0700

    init source

---
+++ ResizeImage/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace ResizeImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnResize = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtDest = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txtSourceFile = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDestFile = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnResizeFile = new System.Windows.Forms.Button();
+            this.txtW1 = new System.Windows.Forms.TextBox();
+            this.txtH1 = new System.Windows.Forms.TextBox();
+            this.txtH2 = new System.Windows.Forms.TextBox();
+            this.txtW2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnResize
+            // 
+            this.btnResize.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnResize.Location = new System.Drawing.Point(546, 62);
+            this.btnResize.Name = "btnResize";
+            this.btnResize.Size = new System.Drawing.Size(107, 25);
+            this.btnResize.TabIndex = 1;
+            this.btnResize.Text = "Resize";
+            this.btnResize.UseVisualStyleBackColor = true;
+            this.btnResize.Click += new System.EventHandler(this.btnResize_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source Folder";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(92, 10);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(561, 20);
+            this.txtSource.TabIndex = 3;
+            // 
+            // txtDest
+            // 
+            this.txtDest.Location = new System.Drawing.Point(92, 36);
+            this.txtDest.Name = "txtDest";
+            this.txtDest.Size = new System.Drawing.Size(561, 20);
+            this.txtDest.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Dest Folder";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 183);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(669, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // txtSourceFile
+            // 
+            this.txtSourceFile.Location = new System.Drawing.Point(93, 93);
+            this.txtSourceFile.Name = "txtSourceFile";
+            this.txtSourceFile.Size = new System.Drawing.Size(560, 20);
+            this.txtSourceFile.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Source File";
+            // 
+            // txtDestFile
+            // 
+            this.txtDestFile.Location = new System.Drawing.Point(93, 119);
+            this.txtDestFile.Name = "txtDestFile";
+            this.txtDestFile.Size = new System.Drawing.Size(561, 20);
+            this.txtDestFile.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Dest File";
+            // 
+            // btnResizeFile
+            // 
+            this.btnResizeFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnResizeFile.Location = new System.Drawing.Point(546, 145);
+            this.btnResizeFile.Name = "btnResizeFile";
+            this.btnResizeFile.Size = new System.Drawing.Size(107, 25);
+            this.btnResizeFile.TabIndex = 13;
+            this.btnResizeFile.Text = "Resize";
+            this.btnResizeFile.UseVisualStyleBackColor = true;
+            this.btnResizeFile.Click += new System.EventHandler(this.btnResizeFile_Click);
+            // 
+            // txtW1
+            // 
+            this.txtW1.Location = new System.Drawing.Point(414, 66);
+            this.txtW1.Name = "txtW1";
+            this.txtW1.Size = new System.Drawing.Size(57, 20);
+            this.txtW1.TabIndex = 14;
+            this.txtW1.Text = "300";
+            this.txtW1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtH1
+            // 
+            this.txtH1.Location = new System.Drawing.Point(483, 65);
+            this.txtH1.Name = "txtH1";
+            this.txtH1.Size = new System.Drawing.Size(57, 20);
+            this.txtH1.TabIndex = 15;
+            this.txtH1.Text = "300";
+            this.txtH1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtH2
+            // 
+            this.txtH2.Location = new System.Drawing.Point(483, 148);
+            this.txtH2.Name = "txtH2";
+            this.txtH2.Size = new System.Drawing.Size(57, 20);
+            this.txtH2.TabIndex = 17;
+            this.txtH2.Text = "300";
+            this.txtH2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtW2
+            // 
+            this.txtW2.Location = new System.Drawing.Point(414, 149);
+            this.txtW2.Name = "txtW2";
+            this.txtW2.Size = new System.Drawing.Size(57, 20);
+            this.txtW2.TabIndex = 16;
+            this.txtW2.Text = "300";
+            this.txtW2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 206);
+            this.Controls.Add(this.txtH2);
+            this.Controls.Add(this.txtW2);
+            this.Controls.Add(this.txtH1);
+            this.Controls.Add(this.txtW1);
+            this.Controls.Add(this.btnResizeFile);
+            this.Controls.Add(this.txtDestFile);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSourceFile);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txtDest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnResize);
+            this.Name = "Form1";
+            this.Text = "Resize Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnResize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtDest;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox txtSourceFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDestFile;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnResizeFile;
+        private System.Windows.Forms.TextBox txtW1;
+        private System.Windows.Forms.TextBox txtH1;
+        private System.Windows.Forms.TextBox txtH2;
+        private System.Windows.Forms.TextBox txtW2;
+
+    }
+}
+
---
+++ ResizeImage/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ResizeImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private ImageCodecInfo GetEncoderInfo(ImageFormat format)
+        {
+            return ImageCodecInfo.GetImageDecoders().SingleOrDefault(c => c.FormatID == format.Guid);
+        }
+        private void ResizeImage(Bitmap image, int Width, int Height, int quality, string filePath)
+        {
+            int sourceWidth = image.Width;
+            int sourceHeight = image.Height;
+            int destX = 0;
+            int destY = 0;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)Width / (float)sourceWidth);
+            nPercentH = ((float)Height / (float)sourceHeight);
+
+            if(nPercentH < nPercentW)
+            {
+                nPercent = nPercentW;
+                destY = (int)((Height - (sourceHeight * nPercent)) / 2);
+            }
+            else
+            {
+                nPercent = nPercentH;
+                destX = (int)((Width - (sourceWidth * nPercent)) / 2);
+            }
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap newImage = new Bitmap(Width, Height, PixelFormat.Format24bppRgb);
+            newImage.SetResolution(72, 72);
+            using(Graphics graphics = Graphics.FromImage(newImage))
+            {
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.DrawImage(image, new Rectangle(destX, destY, destWidth, destHeight), new Rectangle(0, 0, sourceWidth, sourceHeight), GraphicsUnit.Pixel);
+            }
+            ImageCodecInfo imageCodecInfo = GetEncoderInfo(ImageFormat.Jpeg);
+
+            System.Drawing.Imaging.Encoder encoder = System.Drawing.Imaging.Encoder.Quality;
+
+            EncoderParameters encoderParameters = new EncoderParameters(1);
+
+            EncoderParameter encoderParameter = new EncoderParameter(encoder, quality);
+            encoderParameters.Param[0] = encoderParameter;
+            image.Dispose();
+            newImage.Save(filePath, imageCodecInfo, encoderParameters);
+        }
+
+        private void btnResize_Click(object sender, EventArgs e)
+        {
+            if(txtSource.Text != "" && txtDest.Text != "")
+            {
+                string[] files = Directory.GetFiles(txtSource.Text);
+                foreach(string filename in files)
+                {
+                    try
+                    {
+                        Bitmap b = new Bitmap(filename);
+                        if(b != null)
+                        {
+                            ResizeImage(b, Convert.ToInt32(txtW1.Text), Convert.ToInt32(txtH1.Text), 100, txtDest.Text + "/" + Path.GetFileName(filename));
+                        }
+                    }
+                    catch
+                    {
+                    }
+                }
+                MessageBox.Show("Thành công");
+            }
+        }
+
+        private void btnResizeFile_Click(object sender, EventArgs e)
+        {
+            if(txtSourceFile.Text != "" && txtDestFile.Text != "")
+            {
+                Bitmap b = new Bitmap(txtSourceFile.Text);
+                if(b != null)
+                {
+                    ResizeImage(b, Convert.ToInt32(txtW2.Text), Convert.ToInt32(txtH2.Text), 80, txtDestFile.Text);
+                    MessageBox.Show("Thành công");
+                }
+            }
+        }
+    }
+}
